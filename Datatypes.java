1ed2d7b00043b3b48612ed1142c44163d13d162a
Sai4615 <svanisri443363@gmail.com>
2024-11-18 16:46:32 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		int A = 1234567890;
+		long B = 12345678901L;
+		float C = 123456.123456789F;
+		double D = -123.45678;
+		char E = 'L';
+		boolean F = true;
+		System.out.println("The Value of A is:"+ A  );
+		System.out.println("The Value of B or Long:" + B);
+		System.out.println("The Value of E or float:" + E);
+		   
+	 
+	}
+
+}
+
---
+++ Operators.java
@@ -0,0 +1,82 @@
+package day1;
+
+public class Operators {
+	public static void main(String[] args) {		
+//		  // Arithmetic operators
+//		  
+		  int A = 10; int B = 50; System.out.println("A+B:" + (A+B) );
+		  
+		  int C= 2;
+//		  
+//		  System.out.println("Sub:" + (B-A)); System.out.println("Multiple:" + (A*C));
+//		  System.out.println("Division:" + (A/C)); System.out.println("Percentage:" +
+//		  (B%A));
+//		  
+//		  // Comparison operators
+//		  
+		  System.out.println("Greaterthan:" + (A>B)); 
+//		  System.out.println("Lessthan:" +
+//		  (A<B)); System.out.println("Equalto:" + (A==B));
+//		  System.out.println("Not Equal to:" + (A!=C));
+//		  
+		 
+//	Logical
+	
+//	And = && / Or- ||/ Not = !
+// And			
+	int X= 10,Y=20;
+	
+	  if (X>5 && Y>10) { System.out.println("Pass"); }
+	  
+	  // or
+	  
+	  if (X>5 || Y<10) { System.out.println("Pass"); }
+	  
+	  // Not boolean 
+	  
+	  Boolean J= true;
+	  
+	  if (!J) {
+		  System.out.println("Pass"); 
+		  } 
+	  else {
+		  System.out.println("Fail");
+	  
+		  System.out.println("Summa");
+	  
+	  String a = "Karthik";
+	  String b = " Raja";
+	  System.out.println(a+b);
+	  
+//	  int d = 0;
+	  System.out.println(a.replace("K", "M"));
+	  
+	  }
+	
+	}
+}
+	 
+	/*
+	
+	
+//	And / Or 
+//	
+	
+	int M= 10,N=20;
+	
+//	if ((M>5) || (N<10)) {System.out.println("True");
+//}
+
+	
+//Unary
+// increment and decrement prefix only we accept 
+//System.out.println(++M);
+
+//Shorthand - M = M+5 in shorthand  M+=5 both are same
+//
+//M=M+5; System.out.println(M);
+
+M+=7;System.out.println(M);
+}
+}}
+*/
